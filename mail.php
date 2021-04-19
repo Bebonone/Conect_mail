d46efd0e29628ec76db491d6ef70609b96176ee5
Mohamed Omer (Bebon6) <42280148+Bebonone@users.noreply.github.com>
2021-04-19 07:02:52 +0300

    mail

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $number= $_POST['mobile'];
-$to = "mohamed.bes2012@mail.com";
+$to = "mohamed.best2012@mail.com";
 $subject = "Mail From gmail";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message . "\r\n Mobile number =" . $number;
 $headers = "From: noreply@gmail.com" . "\r\n" .
